c9e3fdbf07e5a2afdc38ebdd2c299cdc5049dd5e
Matt <matthiasvstephens@gmail.com>
2021-11-27 00:31:12 +0100

    Create Event and User models

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const EventSchema = new Schema({});
+const EventSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  creation_date: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  attending: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
 
 const Event = mongoose.model('Event', EventSchema);
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,73 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, 'Bad email provided'],
+  },
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+    validate: [({ length }) => length >= 2, 'Username too short.'],
+  },
+  password: {
+    type: String,
+    required: true,
+    // Validation is done within pre hooks
+  },
+  creation_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Event = mongoose.model('User', UserSchema);
+UserSchema.pre('save', async function (next) {
+  try {
+    const regex =
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+    if (!this.password.match(regex)) {
+      return next(new Error('Password failed validation'));
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.pre('findOneAndUpdate', async function (next) {
+  try {
+    if (!this._update.password) return next();
+
+    const regex =
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+    if (!this._update.password.match(regex)) {
+      return next(new Error('New password failed validation'));
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this._update.password = await bcrypt.hash(this._update.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.methods.checkPassword = async function (input) {
+  try {
+    return await bcrypt.compare(input, this.password);
+  } catch (err) {
+    return err;
+  }
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;

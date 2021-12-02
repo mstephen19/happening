2901f4665263b6115f86e67626c8f17507b456b9
isa <5445946+icortes@users.noreply.github.com>
2021-12-02 12:25:53 -0800

    removed useCreateIndex and useFindAndModify from connection.js (no longer necessary), seeding to the database functional

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,35 +1,39 @@
 const db = require('../config/connection');
 const {User, Event} = require('../models');
+const {Types} = require('mongoose');
 
 db.once('open', async () => {
-  await User.deleteMany();
-
-  const users = await User.insertMany([
-    {
-      email: 'email1@happening.com',
-      username: 'test1',
-      password: 'password1234',
-      creation_date: new Date.now(),
-    },
-  ]);
-
-  console.log('users seeded');
-
   await Event.deleteMany();
 
   const events = await Event.insertMany([
     {
-      creator: users[0]._id,
       name: 'San Francisco Zoo & Gardens',
       body: 'The San Francisco Zoo is designed with the underlying belief that nature-focused interaction leads to conservation action. Learning about animals here inspires visitors to care for all wildlife.',
-      creation_date: new Date.now(),
       location: 'San Francisco Bay Area',
       address: 'Sloat Blvd &, Upper Great Hwy, San Francisco, CA 94132',
-      attending: [users[0]._id],
     },
   ]);
 
   console.log('events seeded');
+
+  await User.deleteMany();
+
+  await User.create([
+    {
+      email: 'email1@happening.com',
+      username: 'test1',
+      password: 'Password1234$',
+      events: [Types.ObjectId(events[0]._id)],
+    },
+    {
+      email: 'email2@happening.com',
+      username: 'test2',
+      password: 'Password1234#',
+      events: [Types.ObjectId(events[0]._id)],
+    }
+  ]);
+
+  console.log('users seeded');
 
   process.exit();
 });

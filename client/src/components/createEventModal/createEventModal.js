a312b2762d6cad367dba3dd893104add33342343
alonzofroman <alonzofroman@gmail.com>
2021-12-02 11:38:28 -0800

    Create event form

--- client/src/components/createEventModal/createEventModal.js
+++ client/src/components/createEventModal/createEventModal.js
@@ -7,13 +7,18 @@ export default function CreateEventForm() {
             <h1>Create Event</h1>
             <Form>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Group className="mb-3" controlId="eventName">
+                <Form.Label>Event Name</Form.Label>
+                <Form.Control type="text" placeholder="Concert at the park" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Example textarea</Form.Label>
+            <Form.Group className='mb-3' controlId="eventLocation">
+                <Form.Label>Event Location (Include links to ticket sites)</Form.Label>
+                <Form.Control type='text' placeholder=" 532 S Olive St, Los Angeles, CA 90013" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="eventBody">
+                <Form.Label>Event Description</Form.Label>
                 <Form.Control as="textarea" rows={3} />
             </Form.Group>
 

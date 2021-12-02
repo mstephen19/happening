e907f3fbb9b06268c17f309e6d552de2bd402ae5
alonzofroman <alonzofroman@gmail.com>
2021-12-02 12:29:17 -0800

    Rebass form inputs complete

--- client/src/components/createEventModal/createEventModal.js
+++ client/src/components/createEventModal/createEventModal.js
@@ -1,18 +1,7 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import {
-    Label,
-    Input,
-    Select,
-    Textarea,
-    Radio,
-    Checkbox,
-  } from '@rebass/forms'
-  import {
-      Box,
-      Flex
-  } from 'rebass'
-
+import { Label, Input, Textarea, } from "@rebass/forms";
+import { Box, Flex } from "rebass";
 
 export default function CreateEventForm() {
   const [show, setShow] = useState(false);
@@ -23,71 +12,33 @@ export default function CreateEventForm() {
   return (
     <div>
       <Button variant='primary' onClick={handleShow}>
-        Launch demo modal
+        Create Event
       </Button>
 
       <Modal show={show} onHide={handleClose}>
 
-        <Box
-  as='form'
-  onSubmit={e => e.preventDefault()}
-  py={3}>
-  <Flex mx={-2} mb={3}>
-    <Box width={1/2} px={2}>
-      <Label htmlFor='name'>Name</Label>
-      <Input
-        id='name'
-        name='name'
-        defaultValue='Jane Doe'
-      />
-    </Box>
-    <Box width={1/2} px={2}>
-      <Label htmlFor='location'>Location</Label>
-      <Select
-        id='location'
-        name='location'
-        defaultValue='NYC'>
-        <option>NYC</option>
-        <option>DC</option>
-        <option>ATX</option>
-        <option>SF</option>
-        <option>LA</option>
-      </Select>
-    </Box>
-  </Flex>
-  <Flex mx={-2} flexWrap='wrap'>
-    <Label width={[ 1/2, 1/4 ]} p={2}>
-      <Radio
-        id='beep'
-        name='beep'
-        value='beep'
-        defaultChecked
-      />
-      Beep
-    </Label>
-    <Label width={[ 1/2, 1/4 ]} p={2}>
-      <Radio
-        id='boop'
-        name='beep'
-        value='boop'
-      />
-      Boop
-    </Label>
-    <Label width={[1/2, 1/4]} p={2}>
-      <Checkbox
-        id='remember'
-        name='remember'
-      />
-      Remember Me
-    </Label>
-    <Box px={2} ml='auto'>
-      <Button>
-        Beep
-      </Button>
-    </Box>
-  </Flex>
-</Box>
+        <Box as='form' onSubmit={(e) => e.preventDefault()}  >
+            <Box width={1} px={2}>
+              <Label htmlFor='name'>Event Name</Label>
+              <Input id='eventName' name='name' defaultValue='Concert at the park' />
+            </Box>
 
+            <Box width={1} px={2}>
+              <Label htmlFor='location'>Event Location</Label>
+              <Input id='eventLocation' name='location' defaultValue='532 S Olive St, Los Angeles, CA 90013' />
+            </Box>
+
+            <Box width={1} px={2}>
+              <Label htmlFor='body'>Event Description</Label>
+              <Textarea id='eventBody' name='body' defaultValue='Enter event description. (Provide links if necessary)' />
+            </Box>
+         
+            <Box px={2} ml='auto'>
+              <Button>Submit Event</Button>
+            </Box>
+          
+
+        </Box>
       </Modal>
     </div>
   );

2c575d7e1ebc2b0ca8dbd6b3f602d056632939c7
alonzofroman <alonzofroman@gmail.com>
2021-12-02 11:22:24 -0800

    Added bootstrap to client side

--- client/src/components/createEventModal/createEventModal.js
+++ client/src/components/createEventModal/createEventModal.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function CreateEventForm() {
     return (
         <div>
+            <h1>Create Event</h1>
             <form>
                 
             </form>

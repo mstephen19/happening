02c2be91615a1916030e5d17945a9bcf1755d857
alonzofroman <alonzofroman@gmail.com>
2021-12-02 12:10:33 -0800

    Switched to rebass for form modal

--- client/src/components/createEventModal/createEventModal.js
+++ client/src/components/createEventModal/createEventModal.js
@@ -1,28 +1,94 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import {
+    Label,
+    Input,
+    Select,
+    Textarea,
+    Radio,
+    Checkbox,
+  } from '@rebass/forms'
+  import {
+      Box,
+      Flex
+  } from 'rebass'
+
 
 export default function CreateEventForm() {
-    return (
-        <div>
-            <h1>Create Event</h1>
-            <Form>
+  const [show, setShow] = useState(false);
 
-            <Form.Group className="mb-3" controlId="eventName">
-                <Form.Label>Event Name</Form.Label>
-                <Form.Control type="text" placeholder="Concert at the park" />
-            </Form.Group>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-            <Form.Group className='mb-3' controlId="eventLocation">
-                <Form.Label>Event Location (Include links to ticket sites)</Form.Label>
-                <Form.Control type='text' placeholder=" 532 S Olive St, Los Angeles, CA 90013" />
-            </Form.Group>
+  return (
+    <div>
+      <Button variant='primary' onClick={handleShow}>
+        Launch demo modal
+      </Button>
 
-            <Form.Group className="mb-3" controlId="eventBody">
-                <Form.Label>Event Description</Form.Label>
-                <Form.Control as="textarea" rows={3} />
-            </Form.Group>
+      <Modal show={show} onHide={handleClose}>
 
-            </Form>
-        </div>
-    )
-};
+        <Box
+  as='form'
+  onSubmit={e => e.preventDefault()}
+  py={3}>
+  <Flex mx={-2} mb={3}>
+    <Box width={1/2} px={2}>
+      <Label htmlFor='name'>Name</Label>
+      <Input
+        id='name'
+        name='name'
+        defaultValue='Jane Doe'
+      />
+    </Box>
+    <Box width={1/2} px={2}>
+      <Label htmlFor='location'>Location</Label>
+      <Select
+        id='location'
+        name='location'
+        defaultValue='NYC'>
+        <option>NYC</option>
+        <option>DC</option>
+        <option>ATX</option>
+        <option>SF</option>
+        <option>LA</option>
+      </Select>
+    </Box>
+  </Flex>
+  <Flex mx={-2} flexWrap='wrap'>
+    <Label width={[ 1/2, 1/4 ]} p={2}>
+      <Radio
+        id='beep'
+        name='beep'
+        value='beep'
+        defaultChecked
+      />
+      Beep
+    </Label>
+    <Label width={[ 1/2, 1/4 ]} p={2}>
+      <Radio
+        id='boop'
+        name='beep'
+        value='boop'
+      />
+      Boop
+    </Label>
+    <Label width={[1/2, 1/4]} p={2}>
+      <Checkbox
+        id='remember'
+        name='remember'
+      />
+      Remember Me
+    </Label>
+    <Box px={2} ml='auto'>
+      <Button>
+        Beep
+      </Button>
+    </Box>
+  </Flex>
+</Box>
+
+      </Modal>
+    </div>
+  );
+}
